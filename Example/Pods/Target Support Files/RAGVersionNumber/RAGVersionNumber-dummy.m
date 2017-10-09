78c580361dcfaf8b0283a287e3617375e4542083
Reimar Twelker <reimar.twelker@web.de>
2017-10-09 19:04:36 +0200

    Init

---
+++ Example/Pods/Target Support Files/RAGVersionNumber/RAGVersionNumber-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RAGVersionNumber : NSObject
+@end
+@implementation PodsDummy_RAGVersionNumber
+@end
---
+++ RAGVersionNumber/Classes/RAGVersionNumber.swift
@@ -0,0 +1,9 @@
+//
+//  RAGVersionNumber.swift
+//  Pods
+//
+//  Created by Reimar Twelker on 09.10.17.
+//
+//
+
+import Foundation
