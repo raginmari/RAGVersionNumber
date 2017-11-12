56a3ce5dd43c302d017ed2ed1369cb1f3d7a8135
Reimar Twelker <reimar.twelker@web.de>
2017-11-12 02:00:33 +0100

    Added framework target and shared scheme.

---
+++ Example/Pods/VersionNumber/VersionNumber.h
@@ -0,0 +1,19 @@
+//
+//  VersionNumber.h
+//  VersionNumber
+//
+//  Created by Reimar Twelker on 12.11.17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VersionNumber.
+FOUNDATION_EXPORT double VersionNumberVersionNumber;
+
+//! Project version string for VersionNumber.
+FOUNDATION_EXPORT const unsigned char VersionNumberVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VersionNumber/PublicHeader.h>
+
+
